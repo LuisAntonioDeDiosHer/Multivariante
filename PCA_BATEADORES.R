8a04611dd3690c1ee9183ef9619ef9edba89ba24
LuisAntonioDeDiosHer <101357296+LuisAntonioDeDiosHer@users.noreply.github.com>
2022-05-24 22:03:00 -0500

    Add files via upload

---
+++ PCA_BATEADORES.R
@@ -0,0 +1,132 @@
+#Analisis de componentes principales
+## passo 1 instalar el paquete datos
+install.packages("datos")
+
+##Llamar a la libreria
+library(datos)
+library(dplyr)
+library(MASS)
+##elegir una matriz
+datos::bateadores
+x<- bateadores
+str<-x 
+colnames(x)
+#install.packages("dplyr") # Instalar paquete de manipulación de dataframes
+library(dplyr)
+x<-x[1:300,6:12]
+x
+#X <-select(x, juegos, al_bate, carreras, golpes, dobles, triples, carreras_empujadas, bases_robadas) # Forma simple 2
+#X
+# Definir como n y p el número de flores y
+# variables
+n<-nrow(x)
+p<-ncol(x)
+
+
+# 4.- Generaci?n de un scatterplot
+# de las variables originales
+pairs(x,col="blue", pch=19, 
+      main="Variables originales")
+
+#5.- Obtenci?n de los componentes principales
+# con base en la matriz de covarianza muestral
+mu<-colMeans(x)
+s<-cov(x)
+
+#6.- Obtenci?n de los componentes principales 
+# con base a la matriz de covarianza muestral
+es<-eigen(s)
+es
+
+# 7.- Matriz de auto-valores
+eigen.val<-es$values
+
+# 8.- Matriz de auto-vectores
+eigen.vec<-es$vectors
+
+# Proporci?n de variabilidad para cada vector
+pro.var<-eigen.val/sum(eigen.val)
+
+# Proporci?n de variabilidad acumulada
+pro.var.acum<-cumsum(eigen.val)/sum(eigen.val)
+
+
+# Media de los auto-valores
+mean(eigen.val)
+
+#---------------------------
+# Obtencion de los coeficientes (nuevas variables)
+# 
+#--------------------------
+
+# 1.- Centrar los datos con respecto a la media
+ones<-matrix(rep(1,n),nrow=n, ncol=1)
+
+# 2.- Construccion de la matriz centrada
+X.cen<-as.matrix(x)-ones%*%mu
+X.cen
+
+# 3.- Construccion de la matriz diagonal de las 
+# varianzas
+Dx<-diag(diag(s))
+Dx
+
+# 4.- Construccion de la matriz centrada multiplicada
+# por Dx^1/2
+
+Y<-X.cen%*%solve(Dx)^(1/2)
+Y #datos normalizados
+
+# 5.- Construccion de los coeficientes o scores
+# eigen.vec matriz de autovectores
+scores<-Y%*%eigen.vec
+
+# Nombramos las columnas PC1...PC8
+colnames(scores)<-c("PC1","PC2","PC3","PC4","PC5",
+                    "PC6", "PC7")
+
+# visualizamos
+scores
+
+# Generacion del grafico de los scores
+pairs(scores, main="scores", col="blue", pch=19)
+
+#--------------------------------------
+#  PCA sintetizado
+#-------------------------------------
+
+View(x)
+head(x)
+
+# Aplicar el c?lculo de la varianza a las columnas 
+# 1=filas, 2=columnas
+apply(x, 2, var)
+
+# centrado por la media y escalada por 
+# la desviacion standar (dividir entre sd).
+
+acp<-prcomp(x, center=TRUE, scale=TRUE)
+acp
+
+# Generaci?n del gr?fico screeplot
+plot(acp, type="l")
+
+# Visualizar el resumen
+summary(acp)
+
+# Construcci?n del Biplot
+biplot(acp, scale=0)
+
+# Componente principal calculada
+# Suma del producto de la matriz acp de cada uno
+# de los componentes por el dato de la matriz original
+# por filas
+# filas = 1
+# columnas = 2
+pc1<-apply(acp$rotation[,1]*x, 1, sum)
+pc2<-apply(acp$rotation[,2]*x, 1, sum)
+
+
+x$pc1<-pc1
+x$pc2<-pc2
+
